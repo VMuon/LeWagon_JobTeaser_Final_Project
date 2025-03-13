2d363b7f8534160135c349985da957d1d0200ed3
Valentin MUON <valentin.muon@gmail.com>
2025-03-13 16:03:05 +0000

    Ajout des tables optin_aggregated et candidate_status_aggregated

--- models/staging/jobteaser_lewagon/stg_jobteaser_lewagon__candidate_status_update_cleaned.sql
+++ models/staging/jobteaser_lewagon/stg_jobteaser_lewagon__candidate_status_update_cleaned.sql
@@ -2,10 +2,7 @@ with
 
 source as (
 
-    select * 
-    from {{ source('jobteaser_lewagon', 'candidate_status_update_cleaned') }} as status
-    join {{ ref('stg_jobteaser_lewagon__optin_cleaned') }} as optin
-    using (user_id)
+    select * from {{ source('jobteaser_lewagon', 'candidate_status_update_cleaned') }}
 
 ),
 
@@ -13,19 +10,17 @@ renamed as (
 
     select
         user_id,
-        status.receive_time,
+        receive_time,
         shortlist_id,
         status_update,
         cause,
         school_id,
         current_sign_in_at,
         next_receive_time,
-        response_time_hours,
-        
+        response_time_hours
 
     from source
 
 )
 
-select *
-from renamed
+select * from renamed
